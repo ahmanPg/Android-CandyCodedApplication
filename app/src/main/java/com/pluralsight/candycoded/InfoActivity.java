506eee5d19c0d266e77de7c34f8a941bf914f618
Ahman <ahmanmassoud@gmail.com>
2019-10-29 20:21:49 +0300

    Completed module

--- app/src/main/java/com/pluralsight/candycoded/InfoActivity.java
+++ app/src/main/java/com/pluralsight/candycoded/InfoActivity.java
@@ -1,16 +1,19 @@
 package com.pluralsight.candycoded;
 
+import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.squareup.picasso.Picasso;
 
 public class InfoActivity extends AppCompatActivity {
-
+    private TextView address_tv;
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_info);
 
@@ -21,11 +24,35 @@ public class InfoActivity extends AppCompatActivity {
                 into(candyStoreImageView);
 
 
+        address_tv = (TextView) findViewById(R.id.text_view_address);
+       address_tv.setOnClickListener(new View.OnClickListener() {
+           @Override
+           public void onClick(View view) {
+               createMapIntent();
+           }
+       });
     }
+    public void createMapIntent(){
+
+                // Create a Uri from an intent string. Use the result to create an Intent.
+                Uri gmmIntentUri = Uri.parse("google.streetview:cbll=46.414382,10.013988");
+
+                // Create an Intent from gmmIntentUri. Set the action to ACTION_VIEW
+                Intent mapIntent = new Intent(Intent.ACTION_VIEW, gmmIntentUri);
+                // Make the Intent explicit by setting the Google Maps package
+                mapIntent.setPackage("com.google.android.apps.maps")    ;
+
+                // Attempt to start an activity that can handle the Intent
+                startActivity(mapIntent);
+
+    }
+
+
 
     // ***
     // TODO - Task 2 - Launch the Google Maps Activity
     // ***
+
 
     // ***
     // TODO - Task 3 - Launch the Phone Activity
